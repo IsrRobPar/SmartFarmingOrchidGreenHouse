d914bd20c261b296b6f8b930b0ff1f48fbee0945
IsrRobPar <israel_parma@yahoo.com.br>
2024-04-19 11:33:20 +0100

    TEMPERATUR update veriables

--- src/main/java/TemperatureSensorServer.java
+++ src/main/java/TemperatureSensorServer.java
@@ -4,25 +4,18 @@ import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 import java.util.Random;
 
 public class TemperatureSensorServer extends TemperatureSensorServiceGrpc.TemperatureSensorServiceImplBase{
     private final Random random = new Random();
-    private double temperature = randomTemperature();
 
-    private double randomTemperature() {
-        Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new TimerTask() {
-            @Override
-            public void run() {
-                temperature = random.nextDouble() * 100;
-            }
-        }, 0, 30 * 1000);
-        return temperature;
+    private int randomTemperature() {
+        return (random.nextInt(10));
+
     }
+
+    int temperature = randomTemperature();
 
     @Override
     public void getCurrentTemperature(UnaryTemperatureRequest request, StreamObserver<UnaryTemperatureResponse> responseObserver) {
@@ -36,11 +29,10 @@ public class TemperatureSensorServer extends TemperatureSensorServiceGrpc.Temper
 
     @Override
     public void streamCurrentTemperature(StreamTemperatureRequest request, StreamObserver<StreamTemperatureResponse> responseObserver) {
-        double streamTemperature = randomTemperature();
         Runnable streamingTask = () -> {
             try {
                 while (!Thread.currentThread().isInterrupted()) {
-                    String message = " is: " + streamTemperature + "°C. Current time: " + LocalDateTime.now();
+                    String message = " is: " + temperature + "°C. Current time: " + LocalDateTime.now();
                     StreamTemperatureResponse response = StreamTemperatureResponse.newBuilder()
                             .setMessage(message)
                             .build();
