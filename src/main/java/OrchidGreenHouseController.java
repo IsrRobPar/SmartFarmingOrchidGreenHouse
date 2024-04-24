60bef4952a061bb7e67cdcdee5ffed56001e48f6
IsrRobPar <israel_parma@yahoo.com.br>
2024-04-24 09:11:19 +0100

    CLIENT OrchidGreenHouseController

--- src/main/java/OrchidGreenHouseController.java
+++ src/main/java/OrchidGreenHouseController.java
@@ -1,17 +1,19 @@
 import com.example.grpc.fanService.*;
+import com.example.grpc.recordDataService.*;
 import com.example.grpc.temperatureSensor.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.stub.StreamObserver;
-
 import java.util.Random;
 import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 public class OrchidGreenHouseController {
     private final ManagedChannel channel;
     private final TemperatureSensorConnectionGrpc.TemperatureSensorConnectionStub temperatureSensorStub;
     private final FanServiceGrpc.FanServiceStub fanServiceStub;
+    private final RecordDataServiceGrpc.RecordDataServiceStub recordDataServiceStub;
 
     public OrchidGreenHouseController(String host, int port) {
         this.channel = ManagedChannelBuilder.forAddress(host, port)
@@ -19,6 +21,7 @@ public class OrchidGreenHouseController {
                 .build();
         this.temperatureSensorStub = TemperatureSensorConnectionGrpc.newStub(channel);
         this.fanServiceStub = FanServiceGrpc.newStub(channel);
+        this.recordDataServiceStub = RecordDataServiceGrpc.newStub(channel);
     }
 
     //TEMPERATURE SENSOR (SERVER-SIDE STREAMING GRPC)
@@ -100,6 +103,58 @@ public class OrchidGreenHouseController {
         requestObserver.onNext(request);
     }
 
+    //RECORD DATA SERVICE (CLIENT_SIDE STREAMING GRPC)
+    public void recordDataService() {
+        final CountDownLatch finishLatch = new CountDownLatch(1);
+
+        StreamObserver<RecordSensorDataRequest> requestObserver = recordDataServiceStub.sendRecordData(new StreamObserver<RecordSensorDataResponse>() {
+            @Override
+            public void onNext(RecordSensorDataResponse response) {
+                System.out.println("Received Record Data response: " + response.getMessage());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+                System.err.println("Error in streaming record data: " + t.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("Streaming Record Data request completed");
+            }
+
+        });
+
+        try {
+            // Simulate sending sensor data
+            requestObserver.onNext(RecordSensorDataRequest.newBuilder()
+                    .setTemperature(25)
+                    .setSoilHumidity(60)
+                    .build());
+
+            // Sleep for 1 second between sending each data point
+            Thread.sleep(1000);
+
+            requestObserver.onNext(RecordSensorDataRequest.newBuilder()
+                    .setTemperature(29)
+                    .setSoilHumidity(50)
+                    .build());
+
+            requestObserver.onCompleted();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            finishLatch.await(1, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        channel.shutdown();
+
+    }
 
 
     public static void main(String[] args) {
@@ -109,6 +164,9 @@ public class OrchidGreenHouseController {
 
         OrchidGreenHouseController clientFanService = new OrchidGreenHouseController("localhost", 28100);
         clientFanService.fanService();
+
+        OrchidGreenHouseController clientRecordDataService = new OrchidGreenHouseController("localhost", 28500);
+        clientRecordDataService.recordDataService();
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
